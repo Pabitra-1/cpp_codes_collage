5149697a65195010184b3205d5e8f4bf224a31ff
Pabitra-1 <Pabitrapal49@gmail.com>
2026-06-02 21:58:37 +0530

    add read and wright

---
+++ fileHadeling/readAndWright.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+
+    ifstream fout;
+    ofstream fin;
+    fin.open("data.txt");
+    string s;
+    cout << "Enter string : ";
+    getline(cin, s);
+    fin << s;
+    fin.close();
+    fout.open("data.txt");
+    string text;
+    getline(fout, text);
+    cout << text;
+    fout.close();
+    return 0;
+}
